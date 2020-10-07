d78f85c0406f1e1b90a96d3f05abf5899775c214
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-10-07 16:21:18 +0200

    Taks/Project Extends

--- src/core/Project.java
+++ src/core/Project.java
@@ -2,20 +2,21 @@ package core;
 
 import java.util.List;
 
-public class Project {
+public class Project extends Activity{
 
   private List<Activity> childs;
 
+  public Project(Activity father, String name) {
+    super(father, name);
+  }
+
   public void addActivity(Activity a)
   {
-
 
   }
 
   public void removeActivity(Activity a)
   {
 
-
   }
-
 }
--- src/core/Task.java
+++ src/core/Task.java
@@ -2,10 +2,14 @@ package core;
 
 import java.util.List;
 
-public class Task {
+public class Task extends Activity{
 
   private List<Interval> intervals = null;
   private boolean status;
+
+  public Task(Activity father, String name) {
+    super(father, name);
+  }
 
   public boolean getStatus() {
     return status;
