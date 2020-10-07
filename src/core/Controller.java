7825a9994dd6d3388874ff72f501add841153b5a
Adrián Arnela <adrian.arnela@e-campus.uab.cat>
2020-10-07 16:11:52 +0200

    Estructura classe Controller

---
+++ src/core/Controller.java
@@ -0,0 +1,18 @@
+package core;
+
+public class Controller {
+  private Activity root;
+  private List<Activity> activeTasks;
+
+  public void addTask(String name, String projectName) {}
+
+  public void addProject(String name, String projectName) {}
+
+  public void stopTask(String name) {}
+
+  public void startTask(String name) {}
+
+  private Activity findActivityByName(String Name) {}
+
+  public List<Activity> getActiveTask() {}
+}
