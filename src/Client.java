84f12233307290dfc2c172767bc9aa17fac2c64e
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-10-07 14:58:16 +0200

    first push

---
+++ src/Client.java
@@ -0,0 +1,28 @@
+import java.time.LocalDateTime;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Client implements Observer {
+
+  @Override
+  public void update(Observable o, Object arg) {
+    System.out.println((LocalDateTime) arg);
+  }
+
+  public void start() {
+    Clock c = new Clock();
+    c.addObserver(this);
+    c.start();
+    try {
+      Thread.sleep(1000*10);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    c.cancel();
+  }
+
+  public static void main(String[] args) {
+    Client client = new Client();
+    client.start();
+  }
+}
---
+++ src/Clock.java
@@ -0,0 +1,36 @@
+import java.time.LocalDateTime;
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Clock extends Observable {
+  private Timer timer;
+  private LocalDateTime date;
+  private static Clock clock;
+
+  public Clock() {
+    timer = new Timer();
+  }
+
+  public LocalDateTime getDate(){
+    return date;
+  }
+
+  public void start() {
+    TimerTask tt = new TimerTask() {
+      @Override
+      public void run() {
+        date = LocalDateTime.now();
+        setChanged();
+        notifyObservers(date);
+      }
+    };
+    timer.scheduleAtFixedRate(tt, 0, 2*1000);
+  }
+
+  public void cancel() {
+    timer.cancel();
+  }
+
+
+}
