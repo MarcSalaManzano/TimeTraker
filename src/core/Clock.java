3fd26d255ea7415ffc168e464df9b2c7055bdfcc
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-10-21 18:24:28 +0200

    Comentarios Clock

--- src/core/Clock.java
+++ src/core/Clock.java
@@ -6,7 +6,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /*
-Class Clock que sigue el patron Observer, representando la clase Observable. 
+Class Clock que sigue el patron Observer, representando la clase Observable.
 Este patron de diseño nos permite crear la clase Clock sin que sea necesario que esta conozca a las clases observadoras.
 Tambien sigue el patron creacionalSingleton, ya que solo es necesario un reloj en el programa.
 Notifica a los observadores (interval y Client), en los instantes de tiempo que deben actualizar la información.
@@ -27,6 +27,10 @@ public class Clock extends Observable {
     return date;
   }
 
+  /*
+  Función que crea un TimerTask que se ejecuta cada 2 segundos en un thread separado, este es el encargado de recuperar la fecha actual y
+  avisar a los Observadores.
+   */
   public void start() {
     TimerTask tt = new TimerTask() {
       @Override
@@ -39,10 +43,17 @@ public class Clock extends Observable {
     timer.scheduleAtFixedRate(tt, 0, 2*1000);
   }
 
+  /*
+  Función que para la ejecucion del TimerTask.
+   */
   public void cancel() {
     timer.cancel();
   }
 
+  /*
+  Función necesaria para el patrón Singleton, esta devuelve la instancia del reloj en caso de que exista
+  o la crea y devuelve (Lazy initialization).
+   */
   public static Clock getInstance() {
     if(clock == null) {
       clock = new Clock();
